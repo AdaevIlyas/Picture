ce783eda52225d3903f96fdb65c590378836733b
Ilyas <il_adaev@mail.ru>
2023-03-29 16:25:13 +0600

    fix accordion

--- dist/script.js
+++ dist/script.js
@@ -173,6 +173,11 @@ const accordion = triggersSelector => {
   const btns = document.querySelectorAll(triggersSelector);
   btns.forEach(btn => {
     btn.addEventListener('click', function () {
+      btns.forEach(item => {
+        item.classList.remove('active-style');
+        item.nextElementSibling.classList.remove('active-content');
+        item.nextElementSibling.style.maxHeight = '0px';
+      });
       this.classList.toggle('active-style');
       this.nextElementSibling.classList.toggle('active-content');
       if (this.classList.contains('active-style')) {
